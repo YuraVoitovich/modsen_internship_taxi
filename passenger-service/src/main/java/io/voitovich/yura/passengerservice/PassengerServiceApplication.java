82e6d9fc2d5adee2ed25c00c8afd4874b2b46d85
= <=>
2023-11-08 17:40:16 +0300

    initial commit

---
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/PassengerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.voitovich.yura.passengerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerServiceApplication.class, args);
+    }
+
+}
