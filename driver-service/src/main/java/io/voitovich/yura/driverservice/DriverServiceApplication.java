1352e17744fde537100e23b95a667475b68a67ba
= <=>
2023-11-10 17:30:14 +0300

    create driver service module

---
+++ driver-service/src/main/java/io/voitovich/yura/driverservice/DriverServiceApplication.java
@@ -0,0 +1,13 @@
+package io.voitovich.yura.driverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverServiceApplication.class, args);
+	}
+
+}
---
+++ driver-service/src/test/java/io/voitovich/yura/driverservice/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.voitovich.yura.driverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
