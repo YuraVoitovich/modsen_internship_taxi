b702625bf7339240a1702b9da6286ddb74dd6303
= <=>
2023-11-14 02:43:31 +0300

    add schemas docs

--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/dto/response/PassengerProfilePageResponse.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/dto/response/PassengerProfilePageResponse.java
@@ -1,6 +1,7 @@
 package io.voitovich.yura.passengerservice.dto.response;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.NonNull;
@@ -8,13 +9,18 @@ import lombok.NonNull;
 import java.util.List;
 
 @Builder
+@Schema(name = "PassengerProfilePageResponse", description = "Response with information about the page")
 public record PassengerProfilePageResponse(
+        @Schema(name = "profiles", description = "Passenger profiles")
         @NonNull
         List<PassengerProfileResponse> profiles,
+        @Schema(name = "pageNumber", description = "The page number")
         @Min(1)
         int pageNumber,
+        @Schema(name = "totalElements", description = "The total elements count")
         @Min(0)
         long totalElements,
+        @Schema(name = "totalPages", description = "The total pages count")
         @Min(0)
         int totalPages
 ) {
--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/exceptionhandler/model/ExceptionInfo.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/exceptionhandler/model/ExceptionInfo.java
@@ -1,5 +1,6 @@
 package io.voitovich.yura.passengerservice.exceptionhandler.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
@@ -7,10 +8,16 @@ import org.springframework.http.HttpStatus;
 
 
 @Builder
+@Schema(description = "Exception info")
 public record ExceptionInfo (
+
+    @Schema(name = "code", description = "error status code", example = "400")
     int code,
+
+    @Schema(name = "message", description = "error message", example = "Not found")
     @NonNull
     String message,
+    @Schema(name = "status", description = "error status", example = "BAD_REQUEST")
     @NonNull
     HttpStatus status
 ) {}
