5697dd6c45941882e0a16d4965029107ffe403a4
= <=>
2023-11-18 02:17:13 +0300

    fix getDriverAvailableRides method in repository

--- ride-service/src/main/kotlin/io/voitovich/yura/rideservice/repository/RideRepository.kt
+++ ride-service/src/main/kotlin/io/voitovich/yura/rideservice/repository/RideRepository.kt
@@ -12,7 +12,7 @@ import java.util.UUID
 
 interface RideRepository: JpaRepository<Ride, UUID> {
     @Query(
-      value = "select id, passenger_profile_id as passengerProfileId, start_geo as startGeo, end_geo as endGeo, ST_Distance(geography(start_geo), end_geo) as distance from ride where status = 'ACCEPTED' and st_dwithin(geography(start_geo), :point, :radius)",
+      value = "select id, passenger_profile_id as passengerProfileId, start_geo as startGeo, end_geo as endGeo, ST_Distance(geography(start_geo), :point) as distance from ride where status = 'REQUESTED' and st_dwithin(geography(start_geo), :point, :radius)",
       nativeQuery = true
     )
     fun getDriverAvailableRides(@Param("point") point: Point, @Param("radius") radius: Int): List<RideProjection>
