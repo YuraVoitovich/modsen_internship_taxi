06f9078419aa08ef4a3390439d6e6c7fbc815c9f
= <=>
2023-11-17 13:54:43 +0300

    add start ride destination and end ride destination

--- ride-service/src/main/resources/db/changelog/v-1.0/changelogs/sql-files/create-ride-table.sql
+++ ride-service/src/main/resources/db/changelog/v-1.0/changelogs/sql-files/create-ride-table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS ride (
     id UUID PRIMARY KEY,
     passenger_profile_id UUID NOT NULL,
-    driver_profile_id UUID NOT NULL,
-    start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    driver_rating NUMERIC NOT NULL,
-    passenger_rating NUMERIC NOT NULL
+    driver_profile_id UUID,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    driver_rating NUMERIC,
+    passenger_rating NUMERIC,
+    start_geo geometry not null,
+    end_geo geometry not null
 );
