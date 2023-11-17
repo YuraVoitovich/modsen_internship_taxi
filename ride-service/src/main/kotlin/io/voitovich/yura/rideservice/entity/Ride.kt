dfd64a78602c7b51d2b47a7fb297acc5ad329d5b
= <=>
2023-11-17 13:54:43 +0300

    add start ride destination and end ride destination

--- ride-service/src/main/kotlin/io/voitovich/yura/rideservice/entity/Ride.kt
+++ ride-service/src/main/kotlin/io/voitovich/yura/rideservice/entity/Ride.kt
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.Point
+import org.locationtech.jts.geom.PrecisionModel
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
@@ -13,17 +16,27 @@ import java.util.UUID
 data class Ride(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    var id: UUID,
+    var id: UUID?,
     @Column(name = "passenger_profile_id")
     var passengerProfileId: UUID,
     @Column(name = "driver_profile_id")
-    var driverProfileId: UUID,
+    var driverProfileId: UUID?,
     @Column(name = "start_date")
-    var startDate: LocalDateTime,
+    var startDate: LocalDateTime?,
     @Column(name = "end_date")
-    var endDate: LocalDateTime,
+    var endDate: LocalDateTime?,
     @Column(name = "driver_rating")
-    var driverRating: BigDecimal,
+    var driverRating: BigDecimal?,
     @Column(name = "passenger_rating")
-    var passengerRating: BigDecimal,
-)
+    var passengerRating: BigDecimal?,
+    @Column(name = "start_geo", columnDefinition = "geometry(Point, $SRID", nullable = false)
+    var startPoint: Point,
+    @Column(name = "end_geo", columnDefinition = "geometry(Point, $SRID", nullable = false)
+    var endPoint: Point,
+) {
+    companion object
+    {
+        private const val SRID = 4326;
+
+    }
+}
