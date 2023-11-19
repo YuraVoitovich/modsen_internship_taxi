5de5fd7ee7fcc52f121f894fdabdd1c7570437c2
= <=>
2023-11-19 20:31:30 +0300

    minor fix

--- driver-service/src/main/java/io/voitovich/yura/driverservice/dto/request/DriverProfileUpdateRequest.java
+++ driver-service/src/main/java/io/voitovich/yura/driverservice/dto/request/DriverProfileUpdateRequest.java
@@ -3,6 +3,7 @@ package io.voitovich.yura.driverservice.dto.request;
 import io.voitovich.yura.driverservice.validation.annotations.SortExclude;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
@@ -11,7 +12,7 @@ import java.util.UUID;
 
 @Builder
 public record DriverProfileUpdateRequest(
-        @NotBlank
+        @NotNull
         UUID id,
 
         @SortExclude
