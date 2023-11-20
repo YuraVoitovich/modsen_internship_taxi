e672359bf7e7d7eafe394a80cb912fc6c38ba18f
= <=>
2023-11-20 18:36:26 +0300

    fix save and update method

--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/dto/mapper/PassengerProfileMapper.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/dto/mapper/PassengerProfileMapper.java
@@ -1,21 +1,23 @@
 package io.voitovich.yura.passengerservice.dto.mapper;
 
 
-import io.voitovich.yura.passengerservice.dto.request.PassengerProfileRequest;
+import io.voitovich.yura.passengerservice.dto.request.PassengerProfileUpdateRequest;
+import io.voitovich.yura.passengerservice.dto.request.PassengerSaveProfileRequest;
 import io.voitovich.yura.passengerservice.dto.response.PassengerProfileResponse;
 import io.voitovich.yura.passengerservice.entity.PassengerProfile;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PassengerProfileMapper {
     PassengerProfileMapper INSTANCE = Mappers.getMapper(PassengerProfileMapper.class);
 
-    PassengerProfileRequest toProfileRequest(PassengerProfile passengerProfile);
-
-    PassengerProfile toEntity(PassengerProfileRequest passengerProfileRequest);
+    PassengerProfile fromSaveRequestToEntity(PassengerSaveProfileRequest passengerSaveProfileRequest);
 
     PassengerProfileResponse toProfileResponse(PassengerProfile profile);
+
+    void updateEntityFromUpdateRequest(PassengerProfileUpdateRequest request, @MappingTarget PassengerProfile target);
 
 
 }
