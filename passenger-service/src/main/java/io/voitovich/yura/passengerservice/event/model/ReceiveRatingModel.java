efbcf6f6057c0fb597ad6dde2de30bc221958180
= <=>
2023-11-23 02:17:31 +0300

    fix typo

--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/event/model/ReceiveRatingModel.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/event/model/ReceiveRatingModel.java
@@ -3,7 +3,7 @@ package io.voitovich.yura.passengerservice.event.model;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record RecieveRatingModel(
+public record ReceiveRatingModel(
         UUID raterId,
         UUID ratedId,
         BigDecimal rating
