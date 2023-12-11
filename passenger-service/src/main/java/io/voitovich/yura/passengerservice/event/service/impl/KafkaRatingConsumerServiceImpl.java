dd08d56ecd7d3c4bb242efe1cad5fe9b574e3139
= <=>
2023-12-11 09:34:31 +0300

    add passenger service rating confirmation

--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/event/service/impl/KafkaRatingConsumerServiceImpl.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/event/service/impl/KafkaRatingConsumerServiceImpl.java
@@ -1,28 +1,32 @@
 package io.voitovich.yura.passengerservice.event.service.impl;
 
+import io.voitovich.yura.passengerservice.event.model.ConfirmRatingReceiveModel;
+import io.voitovich.yura.passengerservice.event.service.KafkaConfirmRatingReceiveService;
 import io.voitovich.yura.passengerservice.event.service.KafkaRatingConsumerService;
 import io.voitovich.yura.passengerservice.event.model.ReceiveRatingModel;
 import io.voitovich.yura.passengerservice.service.RatingService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaRatingConsumerServiceImpl implements KafkaRatingConsumerService {
-
-    private final String topicName = "rate_passenger_topic";
 
     private final RatingService ratingService;
 
-    public KafkaRatingConsumerServiceImpl(RatingService ratingService) {
-        this.ratingService = ratingService;
-    }
+    private final KafkaConfirmRatingReceiveService service;
 
     @Override
     public void consumeRating(ReceiveRatingModel model) {
         log.info("Received message from Kafka. Processing rating for passenger with model: {}",
                 model);
         ratingService.saveAndRecalculateRating(model);
+        service.confirmRatingReceive(ConfirmRatingReceiveModel.builder()
+                .rideId(model.rideId())
+                .rating(model.rating())
+                .build());
     }
 
 }
