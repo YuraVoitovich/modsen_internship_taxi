88e89efd91339873d36fb83cd91b24b76c2de206
= <=>
2024-01-10 16:30:47 +0300

    optimize imports

--- api-gateway/src/main/java/io/voitovich/yura/apigateway/controller/FallbackController.java
+++ api-gateway/src/main/java/io/voitovich/yura/apigateway/controller/FallbackController.java
@@ -2,7 +2,6 @@ package io.voitovich.yura.apigateway.controller;
 
 import io.voitovich.yura.apigateway.model.ServiceFallbackResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- api-gateway/src/test/java/io/voitovich/yura/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/io/voitovich/yura/apigateway/ApiGatewayApplicationTests.java
@@ -1,7 +1,6 @@
 package io.voitovich.yura.apigateway;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
