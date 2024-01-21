9f3626ce6093217feb2a76d4c1376ab6db4be64d
= <=>
2024-01-21 19:16:45 +0300

    refactor build-scripts

--- build-script.bat
+++ build-script.bat
@@ -1,7 +1,7 @@
-call ride-service-build-script.bat
+call ride-service-build-script.bat -DskipTests
 cd ../
-call passenger-service-build-script.bat
+call passenger-service-build-script.bat -DskipTests
 cd ../
-call driver-service-build-script.bat
+call driver-service-build-script.bat -DskipTests
 cd ../
 call docker-compose up -d
--- passenger-service-build-script.bat
+++ passenger-service-build-script.bat
@@ -1,4 +1,4 @@
 cd passenger-service
 
-call mvn clean package
+call mvn clean package %1
 call docker build -t passenger-service .
