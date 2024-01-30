a29d46be713fb02f07c80a06af668a456063dc31
= <=>
2024-01-31 02:40:09 +0300

    implement passenger-service security

---
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/controller/PassengerProfile.java
@@ -0,0 +1,124 @@
+package io.voitovich.yura.passengerservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.voitovich.yura.passengerservice.dto.request.PassengerProfileUpdateRequest;
+import io.voitovich.yura.passengerservice.dto.request.PassengerSaveProfileRequest;
+import io.voitovich.yura.passengerservice.dto.response.PassengerProfilePageResponse;
+import io.voitovich.yura.passengerservice.dto.response.PassengerProfileResponse;
+import io.voitovich.yura.passengerservice.dto.response.PassengerProfilesResponse;
+import io.voitovich.yura.passengerservice.exceptionhandler.model.ExceptionInfo;
+import io.voitovich.yura.passengerservice.exceptionhandler.model.ValidationExceptionInfo;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+public interface PassengerProfile {
+
+    @Operation(description = "Get passenger profile page")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Passenger profile page returned"),
+            @ApiResponse(responseCode = "404", description = "Bad request body data",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
+                    })
+    })
+    PassengerProfilePageResponse getProfilePage(
+            @RequestParam(name = "pageNumber") int pageNumber,
+            @RequestParam(name = "pageSize") int pageSize,
+            @RequestParam(name = "orderBy") String orderBy);
+
+    @Operation(description = "Get passenger profile by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Passenger profile found",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = PassengerProfileResponse.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "Passenger profile not found",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid id format",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    })
+    })
+    PassengerProfileResponse getProfileById(@PathVariable(name = "id") String id);
+
+
+    @Operation(description = "Update passenger profile")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Passenger profile updated"),
+            @ApiResponse(responseCode = "404", description = "Bad request body data",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "409", description = "Passenger phone number is not unique",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid id format",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    })
+    })
+    @PostMapping("/profile")
+    @ResponseStatus(HttpStatus.OK)
+    PassengerProfileResponse updateProfile(@Valid @RequestBody PassengerProfileUpdateRequest request);
+
+
+    @Operation(description = "Save passenger profile")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Passenger profile created"),
+            @ApiResponse(responseCode = "404", description = "Bad request body data",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "409", description = "Passenger phone number is not unique",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid id format",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    })
+    })
+    PassengerProfileResponse saveProfile(@Valid @RequestBody PassengerSaveProfileRequest passengerSaveProfileRequest);
+
+
+    @Operation(description = "Delete passenger profile by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Passenger profile deleted"),
+            @ApiResponse(responseCode = "404", description = "Passenger profile not found",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid id format",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionInfo.class))
+                    })
+    })
+    void deleteProfile(@PathVariable(name = "id") String id);
+    PassengerProfilesResponse getByIds(@PathVariable(name = "ids") List<UUID> uuids);
+
+}
--- passenger-service/src/main/java/io/voitovich/yura/passengerservice/controller/PassengerProfileController.java
+++ passenger-service/src/main/java/io/voitovich/yura/passengerservice/controller/PassengerProfileController.java
@@ -1,11 +1,6 @@
 package io.voitovich.yura.passengerservice.controller;
 
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.voitovich.yura.passengerservice.dto.request.PassengerProfilePageRequest;
 import io.voitovich.yura.passengerservice.dto.request.PassengerProfileUpdateRequest;
@@ -13,15 +8,22 @@ import io.voitovich.yura.passengerservice.dto.request.PassengerSaveProfileReques
 import io.voitovich.yura.passengerservice.dto.response.PassengerProfilePageResponse;
 import io.voitovich.yura.passengerservice.dto.response.PassengerProfileResponse;
 import io.voitovich.yura.passengerservice.dto.response.PassengerProfilesResponse;
-import io.voitovich.yura.passengerservice.exceptionhandler.model.ExceptionInfo;
-import io.voitovich.yura.passengerservice.exceptionhandler.model.ValidationExceptionInfo;
 import io.voitovich.yura.passengerservice.service.PassengerProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static io.voitovich.yura.passengerservice.controller.utils.UUIDUtils.getUUIDFromString;
@@ -30,21 +32,13 @@ import static io.voitovich.yura.passengerservice.controller.utils.UUIDUtils.getU
 @Tag(name = "Passenger profile controller", description = "Passenger profile API")
 @RequestMapping("api/passenger")
 @RequiredArgsConstructor
-public class PassengerProfileController {
+public class PassengerProfileController implements PassengerProfile{
     private final PassengerProfileService profileService;
 
-    @Operation(description = "Get passenger profile page")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Passenger profile page returned"),
-            @ApiResponse(responseCode = "404", description = "Bad request body data",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
-                    })
-    })
+    @PreAuthorize("hasRole('ROLE_modsen-admin')")
     @GetMapping("/profile")
     @ResponseStatus(HttpStatus.OK)
-    PassengerProfilePageResponse getProfilePage(
+    public PassengerProfilePageResponse getProfilePage(
                                                 @RequestParam(name = "pageNumber") int pageNumber,
                                                 @RequestParam(name = "pageSize") int pageSize,
                                                 @RequestParam(name = "orderBy") String orderBy) {
@@ -56,107 +50,40 @@ public class PassengerProfileController {
                 .build());
     }
 
-    @Operation(description = "Get passenger profile by id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Passenger profile found",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = PassengerProfileResponse.class))
-                    }),
-            @ApiResponse(responseCode = "404", description = "Passenger profile not found",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Invalid id format",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    })
-    })
     @GetMapping("/profile/{id}")
     @ResponseStatus(HttpStatus.OK)
-    PassengerProfileResponse getProfileById(@PathVariable(name = "id") String id) {
+    @PreAuthorize("hasRole('ROLE_modsen-user')")
+    @PostAuthorize("returnObject.id() == authentication.principal.claims['sub']")
+    public PassengerProfileResponse getProfileById(@PathVariable(name = "id") String id) {
         return profileService.getProfileById(getUUIDFromString(id));
     }
 
-
-    @Operation(description = "Update passenger profile")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Passenger profile updated"),
-            @ApiResponse(responseCode = "404", description = "Bad request body data",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "409", description = "Passenger phone number is not unique",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Invalid id format",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    })
-    })
     @PostMapping("/profile")
     @ResponseStatus(HttpStatus.OK)
-    PassengerProfileResponse updateProfile(@Valid @RequestBody PassengerProfileUpdateRequest request) {
+    @PreAuthorize("hasRole('ROLE_modsen-user')")
+    public PassengerProfileResponse updateProfile(@Valid @RequestBody PassengerProfileUpdateRequest request) {
         return profileService.updateProfile(request);
     }
 
-
-    @Operation(description = "Save passenger profile")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Passenger profile created"),
-            @ApiResponse(responseCode = "404", description = "Bad request body data",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ValidationExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "409", description = "Passenger phone number is not unique",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Invalid id format",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    })
-    })
     @PutMapping("/profile")
     @ResponseStatus(HttpStatus.CREATED)
-    PassengerProfileResponse saveProfile(@Valid @RequestBody PassengerSaveProfileRequest passengerSaveProfileRequest) {
+    @PreAuthorize("hasRole('ROLE_modsen-admin')")
+    public PassengerProfileResponse saveProfile(@Valid @RequestBody PassengerSaveProfileRequest passengerSaveProfileRequest) {
         return profileService.saveProfile(passengerSaveProfileRequest);
     }
 
-
-    @Operation(description = "Delete passenger profile by id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Passenger profile deleted"),
-            @ApiResponse(responseCode = "404", description = "Passenger profile not found",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Invalid id format",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ExceptionInfo.class))
-                    })
-    })
     @DeleteMapping("/profile/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void deleteProfile(@PathVariable(name = "id") String id) {
+    @PreAuthorize("hasRole('ROLE_modsen-user')")
+    public void deleteProfile(@PathVariable(name = "id") String id) {
         profileService.deleteProfile(getUUIDFromString(id));
     }
 
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/profiles/{ids}")
-    private PassengerProfilesResponse getByIds(@PathVariable(name = "ids") List<UUID> uuids) {
+    @PreAuthorize("hasRole('ROLE_modsen-admin')")
+    public PassengerProfilesResponse getByIds(@PathVariable(name = "ids") List<UUID> uuids) {
         return profileService.getByIds(uuids);
     }
 }
